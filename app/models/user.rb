d3bc7e4e2d339cbc2ecf82f89e34799de7694a8c
Marie Ladwig <marie_ladwig@icloud.com>
2019-11-26 22:29:59 +0100

    add authorization to view activities of others

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   mount_uploader :photo, PhotoUploader
   has_many :selections
-  has_many :restaurants, through: :selections
-  has_many :friendships_as_asker, source: :friendships, foreign_key: :asker_id
-  has_many :friendships_as_receiver, source: :friendships, foreign_key: :receiver_id
+  has_many :friendships_as_asker, source: :friendships, foreign_key: :asker_id, class_name: "Friendship"
+  has_many :friendships_as_receiver, source: :friendships, foreign_key: :receiver_id, class_name: "Friendship"
+  has_many :receivers, through: :friendships_as_asker
+  has_many :askers, through: :friendships_as_receiver
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
