cab722a19fae2a88b24d303516214609cb23b000
Paul Ladwig <ladwigpaul@icloud.com>
2019-11-26 22:10:41 +0100

    search

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,14 @@
 class Restaurant < ApplicationRecord
+  searchkick word_start: [:cuisine, :name]
+  scope :search_import, -> { includes(:selections) }
   has_many :selections
   has_many :users, through: :selections
 
   mount_uploader :photo, PhotoUploader
+
+  def search_data
+    { name: name,
+      cuisine: cuisine
+    }
+  end
 end
