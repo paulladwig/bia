5d48025f5b08531f63be19c46a252f7b645c5aa9
Marie Ladwig <marie_ladwig@icloud.com>
2019-11-27 15:01:37 +0100

    add modal so that it can open

--- app/javascript/components/modal.js
+++ app/javascript/components/modal.js
@@ -1,1 +1,11 @@
-modal.js
+const recommendation_modal = () => {
+  const modal = document.getElementById('recommend-modal');
+  const openBtn = document.getElementById('open-modal');
+  const closeBtn = document.querySelector('.modal-close');
+  openBtn.addEventListener('click', (event) => {
+    modal.style.display="block";
+  });
+};
+
+export { recommendation_modal}
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,4 @@
 import "bootstrap";
+import { recommendation_modal } from '../components/modal'
+
+recommendation_modal()
