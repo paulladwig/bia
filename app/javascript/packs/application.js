0afdd63450b76f3b662eac053f20f8cab2040041
Joris Kuehnast <joris@kuehnast.one>
2019-11-28 15:08:32 +0100

    arrow down dropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@ import {addLinkInNavbarToggle} from '../pages/home.js';
 addLinkInNavbarToggle();
 import { recommendation_modal } from '../components/modal'
 import { locate_user } from '../components/locator';
+import { turn_dropdown_icon } from '../pages/show';
 
+turn_dropdown_icon();
 recommendation_modal();
 locate_user()
