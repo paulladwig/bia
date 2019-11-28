d750e1b5fc17ced624e816d5893ebb1d79de71c0
Paul Ladwig <ladwigpaul@icloud.com>
2019-11-28 23:08:38 +0100

    add filters on index page

--- app/javascript/plugins/clickboard.js
+++ app/javascript/plugins/clickboard.js
@@ -2,23 +2,23 @@
 const copyToClipboard = () => {
 
   var copyTextareaBtn = document.querySelector('.copy-button');
+  if (copyTextareaBtn) {
+    copyTextareaBtn.addEventListener('click', function(event) {
+      var copyTextarea = document.querySelector('.copy-area');
+      copyTextarea.select();
 
-  copyTextareaBtn.addEventListener('click', function(event) {
-    var copyTextarea = document.querySelector('.copy-area');
-    copyTextarea.select();
-
-    try {
-      var successful = document.execCommand('copy');
-      var msg = successful ? 'Copied! ' : 'Oops, unable to copy';
-      console.log('Copying text command was ' + msg);
-      const messageField = document.querySelector('.message-field');
-      console.log(messageField);
-      messageField.innerText = msg;
-    } catch (err) {
-      console.log('Oops, unable to copy');
-    }
-  });
-
+      try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'Copied! ' : 'Oops, unable to copy';
+        console.log('Copying text command was ' + msg);
+        const messageField = document.querySelector('.message-field');
+        console.log(messageField);
+        messageField.innerText = msg;
+      } catch (err) {
+        console.log('Oops, unable to copy');
+      }
+    });
+  }
 };
 
 export {copyToClipboard};
