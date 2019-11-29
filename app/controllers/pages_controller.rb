63e64e4a911e7e3189aa59e4197f1f2514f27e46
Paul Ladwig <ladwigpaul@icloud.com>
2019-11-29 11:08:16 +0100

    add redirect

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if current_user
+      redirect_to discover_path
+    end
   end
 
   def search
