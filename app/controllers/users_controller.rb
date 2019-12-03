3cc492bd4f064a6e34c56a6191c4321c59138ccc
Paul Ladwig <ladwigpaul@icloud.com>
2019-12-03 22:25:22 +0100

    commenced fixing the user show bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,33 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     authorize @user
-    @friendship = Friendship.where(asker_id: current_user.id, receiver_id: @user.id).first
-    query = "*"
-    options = {fields: ["name"], per_page: 24, page: params[:page]}
-    if show_params[:selection] == "bookmarked"
-      options[:where] = {id: Restaurant.user_restaurants_bookmarked(@user, "id")}
-    elsif show_params[:selection] == "recommended"
-      options[:where] = {id: Restaurant.user_restaurants_recommended(@user, "id")}
-    else
-      options[:where] = {id: Restaurant.user_restaurants(@user, "id")}
+    if show_params[:contact].present?
+      if show_params[:contacts] == "follwoing"
+        @friendships = Friendship.where(asker: @user, active: 1).page(params[:page])
+      elsif show_params[:contacts] == "followers"
+        @friendships = Friendship.where(receiver: @user, active: 1).page(params[:page])
+      end
+        @current_page = @friendships.current_page
+        @total_pages = @friendships.total_pages
     end
-    @own_restaurants = policy_scope(Restaurant).search(query, options)
-    @current_page = @own_restaurants.current_page
-    @total_pages = @own_restaurants.total_pages
+
+    if show_params[:selection].present? || show_params.empty?
+      @friendship = Friendship.where(asker_id: current_user.id, receiver_id: @user.id).first
+      query = "*"
+      options = {fields: ["name"], per_page: 24, page: params[:page]}
+      if show_params[:selection] == "bookmarked"
+        options[:where] = {id: Restaurant.user_restaurants_bookmarked(@user, "id")}
+      elsif show_params[:selection] == "recommended"
+        options[:where] = {id: Restaurant.user_restaurants_recommended(@user, "id")}
+      else
+        options[:where] = {id: Restaurant.user_restaurants(@user, "id")}
+      end
+
+      @own_restaurants = policy_scope(Restaurant).search(query, options)
+      @current_page = @own_restaurants.current_page
+      @total_pages = @own_restaurants.total_pages
+    end
+
     respond_to do |format|
       format.html { render 'show' }
       format.js
