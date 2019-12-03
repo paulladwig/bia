5c727b14acb673b6cf3499af4907906bc072b04d
Marie Ladwig <marie_ladwig@icloud.com>
2019-12-03 14:48:19 +0100

    create new share upon submit

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -2,7 +2,6 @@ class SharesController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @share = Share.create(user: current_user, restaurant: @restaurant)
-    redirect_to restaurant_path(@restaurant)
     authorize @share
   end
 end
--- app/policies/share_policy.rb
+++ app/policies/share_policy.rb
@@ -4,4 +4,8 @@ class SharePolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create
+    true
+  end
 end
