e63019f545d458a6d02d6266a6afa68909e12315
Paul Ladwig <ladwigpaul@icloud.com>
2019-12-03 17:14:57 +0100

    seed fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_112723) do
+ActiveRecord::Schema.define(version: 2019_12_03_130444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_112723) do
     t.text "description"
     t.float "longitude"
     t.float "latitude"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
