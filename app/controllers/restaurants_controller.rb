b61f513002959ee78021c2e35946d6aa933b6412
Paul Ladwig <ladwigpaul@icloud.com>
2019-12-04 17:09:35 +0100

    recents in featured

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -108,16 +108,7 @@ class RestaurantsController < ApplicationController
     @results = Kaminari.paginate_array(@results).page(params[:page]).per(18)
     @current_page = @results.current_page
     @total_pages = @results.total_pages
-    all_shares_ordered = Share.where(user: current_user).order(created_at: :desc)
-    all_bookmarked_orders = Selection.where(user: current_user, bookmarked: true, recommended: nil).order(created_at: :desc)
-    all_recent = all_shares_ordered + all_bookmarked_orders
-    all_recent_ordered = all_recent.sort_by { |activity| activity.updated_at }.reverse_each
-    @six_recent = all_recent_ordered.reject do |activity|
-        if !activity.restaurant.selections.find_by(user: current_user).nil?
-          activity.restaurant.selections.find_by(user: current_user).recommended == true
-        end
-      end
-    @six_recent = @six_recent.map { |activity| activity.restaurant}.uniq.first(6)
+    @six_recent = Share.recents(current_user)
   end
 
   def create
--- app/models/share.rb
+++ app/models/share.rb
@@ -1,4 +1,17 @@
 class Share < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
+
+  def self.recents(current_user)
+    all_shares_ordered = self.where(user: current_user).order(created_at: :desc)
+    all_bookmarked_orders = Selection.where(user: current_user, bookmarked: true, recommended: nil).order(created_at: :desc)
+    all_recent = all_shares_ordered + all_bookmarked_orders
+    all_recent_ordered = all_recent.sort_by { |activity| activity.updated_at }.reverse_each
+    six_recent = all_recent_ordered.reject do |activity|
+        if !activity.restaurant.selections.find_by(user: current_user).nil?
+          activity.restaurant.selections.find_by(user: current_user).recommended == true
+        end
+      end
+    six_recent = six_recent.map { |activity| activity.restaurant}.uniq.first(6)
+  end
 end
